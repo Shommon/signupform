2b67206c397402508b42488210fd85c245d20f70
Shommon <stai2@gmu.edu>
2024-01-26 18:34:25 -0500

    Added: requirement text info Added: Password input and confirm shows red background  when unmatched and green when matched Added: style.css .match, .error, .metlength class for styling password inputs Modified: style.css max width for columns, when adding requirement text it wont lengthen the input box Feature not Added: When inputing password, shows user red text that the password hasnt met requirements, green when it does

--- password_match.js
+++ password_match.js
@@ -1,21 +1,80 @@
 const passwordinput = document.querySelector('#password');
 const passwordconfirm = document.querySelector('#password-confirm');
+const col3 = document.querySelector('.col3');
+var requirementinfo = document.createElement('p');
+
+requirementinfo.id = 'requirementText'
+requirementinfo.textContent = 'Password Must contain 5-20 characters'
 
 
-function checkInputContent () {
-    var input = passwordinput.value;
-    if (input !== ''){
-        console.log('not empty');
-    } else {
-        console.log('empty');
+passwordinput.addEventListener('input', (e) => {
+    addLengthRequirementText(e);
+    // checkInputLength();
+
+})
+passwordconfirm.addEventListener('input', () => {
+    checkMatching();
+})
+
+// passwordconfirm.addEventListener('blur', )
+
+
+
+//Handle Password Matching
+    function checkMatching() {
+        if (passwordinput.value == passwordconfirm.value) {
+            passwordMatch();
+            console.log('Password Matches');
+            passwordMatch();
+        } else {
+            passwordNoMatch();
+            console.log('No Match');
+        }
     }
-}
 
-function passwordNoMatch() {
-    //Add Red Color to Input Box
-    //Add Div under input to warn not matching
-}
+    function passwordNoMatch() {
+        passwordinput.classList.remove('match')
+        passwordinput.classList.add('error')
 
-function passwordMatch() {
-    // Add Green Color to Input Box
-}
+        passwordconfirm.classList.remove('match')
+        passwordconfirm.classList.add('error')
+    }
+
+    function passwordMatch() {
+        passwordinput.classList.remove('error')
+        passwordinput.classList.add('match')
+
+        passwordconfirm.classList.remove('error')
+        passwordconfirm.classList.add('match')
+    }
+
+// col3.appendChild(requirementinfo);
+
+//Handle Length Requirement
+    function addLengthRequirementText(element) {
+        // console.log(element.target.id)
+        var parentElement = element.target
+
+
+    }
+
+    function checkInputLength() {
+        var input = passwordinput.value;
+        if (input.length >= 5){
+            console.log('Met Requirement');
+            metLengthRequirement();
+        } else {
+            console.log('Has Not Met Requirement');
+            notMetLengthRequirement();
+        }
+    }
+
+    function metLengthRequirement() {
+        requirementinfo.classlist.remove('error');
+        requirementinfo.classList.add('metlength');
+    }
+
+    function notMetLengthRequirement() {
+        requirementinfo.classlist.remove('metlength');
+        requirementinfo.classList.add('error');
+    }
